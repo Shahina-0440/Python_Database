189eb57c166bcdcd3d3c6c6395f193f55023258d
Shahina-0440 <achukatlashahina@gmail.com>
2023-11-26 14:50:12 +0530

    tst

--- Desktop/shahin/python/Database/CRUD/MongoDB/insert.py
+++ Desktop/shahin/python/Database/CRUD/MongoDB/insert.py
@@ -2,5 +2,5 @@ import pymongo
 client=pymongo.MongoClient()
 db=client["CRUDpymongoDatabase"]
 coll=db["CRUD"]
-data_to_insert = {"name": "John Doe", "age": 25, "city": "New York"}
-result = coll.insert_one(data_to_insert)
+data_to_insert = {"name": "John Doe", "age": 25, "city": "New York","avail":True}
+coll.insert_one(data_to_insert)
